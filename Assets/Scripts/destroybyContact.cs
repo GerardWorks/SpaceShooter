e4f2374829a7bb40883152086772232307e9d8f2
Gerard Ho <gerard.ho@live.com.au>
2016-09-29 22:04:05 +0800

    works fine now complete game

--- Assets/Scripts/destroybyContact.cs
+++ Assets/Scripts/destroybyContact.cs
@@ -1,24 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class destroybyContact : MonoBehaviour {
-
+public class DestroyByContact : MonoBehaviour
+{
 	public GameObject explosion;
-	public GameObject playerdeath;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
 
-	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Boundary") {
-			return;
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
 		}
-		Destroy(other.gameObject);
-		Destroy (gameObject);
-		Instantiate (explosion, transform.position, transform.rotation);
-		if (other.tag == "Player") {
-			Instantiate (playerdeath, other.transform.position, other.transform.rotation);
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
 		}
-
 	}
 
-
-
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}
+		gameController.AddScore (scoreValue);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
 }
--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class gameController : MonoBehaviour {
+public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
@@ -10,10 +10,37 @@ public class gameController : MonoBehaviour {
 	public float startWait;
 	public float waveWait;
 
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+	private int score;
+
+	private bool gameOver;
+	private bool restart;
+
 	void Start ()
 	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
+
 	}
+
+	void Update()
+	{
+		if (restart == true) 
+		{
+			if(Input.GetKeyDown(KeyCode.R))
+				{
+					Application.LoadLevel(Application.loadedLevel);
+				}
+		}
+	}
+
 
 
 	IEnumerator SpawnWaves ()
@@ -30,6 +57,31 @@ public class gameController : MonoBehaviour {
 
 			}
 			yield return new WaitForSeconds (waveWait);
+			if (gameOver == true)
+			{
+				restartText.text = "PRESS R FOR RESTART";
+				restart = true;
+				break;
+			}
 		}
 	}
+
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+
+	}
+
 }
