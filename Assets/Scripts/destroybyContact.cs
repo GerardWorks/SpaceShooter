564231e306139e57c15a240a6f77b5fad29610cd
Gerard Ho <gerard.ho@live.com.au>
2016-09-29 16:01:18 +0800

    updated again

---
+++ Assets/Scripts/destroybyContact.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroybyContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerdeath;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary") {
+			return;
+		}
+		Destroy(other.gameObject);
+		Destroy (gameObject);
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerdeath, other.transform.position, other.transform.rotation);
+		}
+
+	}
+
+
+
+}
